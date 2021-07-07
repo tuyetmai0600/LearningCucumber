c62a87c8621eeb3d14b56b525e9a45f3da4dfd47
tuyetmai0600 <tuyetmai0600@gmail.com>
2021-07-07 19:24:12 -0400

    LearningCumcumber firstday

---
+++ src/features/sample2.feature
@@ -0,0 +1,10 @@
+Feature: This feature is to test sign in
+
+Scenario: This Scenario cover positive signin testing
+
+Given I am in the signin page
+When Insert valid username in username text box
+And Insert valid password  in password text box
+Then user shouble be in the user homepage
+
+ 
---
+++ src/stepDefinition/StepDefinition.java
@@ -0,0 +1,30 @@
+package stepDefinition;
+
+import cucumber.api.java.en.*;
+
+public class StepDefinition {
+
+
+	@Given("^I am in the signin page$")
+	public void i_am_in_the_signin_page() throws Throwable {
+	 
+	}
+
+	@When("^Insert valid username in username text box$")
+	public void insert_valid_username_in_username_text_box() throws Throwable {
+	    
+	}
+
+	@When("^Insert valid password  in password text box$")
+	public void insert_valid_password_in_password_text_box() throws Throwable {
+	    
+	}
+
+	@Then("^user shouble be in the user homepage$")
+	public void user_shouble_be_in_the_user_homepage() throws Throwable {
+	   
+	}
+
+
+
+}
